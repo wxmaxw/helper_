143c4460367157918a3b2c50fc1dc54ab32083c2
wxmaxw <mrvygz01@hotmail.com>
2024-04-14 16:50:21 +0300

    my first step

---
+++ src/pages/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import { SafeAreaView,View,Text } from "react-native";
+
+function App(){
+  return(
+    <SafeAreaView>
+      <View>
+        <Text> Hello world </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
